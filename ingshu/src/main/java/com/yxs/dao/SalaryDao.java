524927c76c8a200a4eaf20742ff9ae8dcc046b18
'cuining' <'445274402@qq.com'>
2017-09-26 11:34:16 +0800

    修改了salary类

--- ingshu/src/main/java/com/yxs/dao/SalaryDao.java
+++ ingshu/src/main/java/com/yxs/dao/SalaryDao.java
@@ -2,8 +2,11 @@ package com.yxs.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.yxs.bean.SalaryBean;
 
+@Repository
 public interface SalaryDao {
 	/**
 	 * 查询工资表信息
